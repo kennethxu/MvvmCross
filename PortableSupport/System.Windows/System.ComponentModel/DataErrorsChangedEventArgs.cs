6331aa4c627726e9e6486ee1877327c828b00ab9
Kenneth Xu <github@homexu.com>
2013-04-22 23:22:02 -0400

    Solution to build the system assemblies to support portable class library.

--- PortableSupport/System.Windows/System.ComponentModel/DataErrorsChangedEventArgs.cs
+++ PortableSupport/System.Windows/System.ComponentModel/DataErrorsChangedEventArgs.cs
@@ -5,18 +5,36 @@
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
+// ReSharper disable CheckNamespace
 namespace System.ComponentModel
 // ReSharper restore CheckNamespace
 {
+    /// <summary>
+    /// Provides data for the <see cref="INotifyDataErrorInfo.ErrorsChanged"/> event.
+    /// </summary>
     public sealed class DataErrorsChangedEventArgs : EventArgs
     {
         private readonly string _propertyName;
 
+        /// <summary>
+        /// Initializes a new instance of the DataErrorsChangedEventArgs class. 
+        /// </summary>
+        /// <param name="propertyName">
+        /// The name of the property for which the errors changed, or <code>null</code> or <see cref="String.Empty">Empty</see> 
+        /// for entity-level errors.
+        /// </param>
         public DataErrorsChangedEventArgs(string propertyName)
         {
             _propertyName = propertyName;
         }
 
+        /// <summary>
+        /// Gets the name of the property that has an error.
+        /// </summary>
+        /// <value>
+        /// The name of the property for which the errors changed, or <code>null</code> or <see cref="String.Empty">Empty</see> 
+        /// for entity-level errors.
+        /// </value>
         public string PropertyName
         {
             get { return _propertyName; }
--- PortableSupport/System.Windows/System.ComponentModel/INotifyDataErrorInfo.cs
+++ PortableSupport/System.Windows/System.ComponentModel/INotifyDataErrorInfo.cs
@@ -6,18 +6,52 @@
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
 using System.Collections;
+using System.Collections.Specialized;
 
 // ReSharper disable CheckNamespace
-
 namespace System.ComponentModel
 // ReSharper restore CheckNamespace
 {
+    /// <summary>
+    /// Defines members that data entity classes can implement to provide custom synchronous and asynchronous validation support.
+    /// </summary>
+    /// <remarks>
+    /// This interface enables data entity classes to implement custom validation rules and expose validation results 
+    /// asynchronously. This interface also supports custom error objects, multiple errors per property, cross-property errors, 
+    /// and entity-level errors. Cross-property errors are errors that affect multiple properties. You can associate these errors 
+    /// with one or all of the affected properties, or you can treat them as entity-level errors. Entity-level errors are errors 
+    /// that either affect multiple properties or affect the entire entity without affecting a particular property.
+    /// </remarks>
     public interface INotifyDataErrorInfo
     {
+        /// <summary>
+        /// Occurs when the validation errors have changed for a property or for the entire entity.
+        /// </summary>
+        /// <remarks>
+        /// The implementing class should raise this event on the user interface thread whenever the GetErrors return value changes, 
+        /// even if the return value implements <see cref="INotifyCollectionChanged"/>.
+        /// </remarks>
         event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
+        /// <summary>
+        /// Gets the validation errors for a specified property or for the entire entity.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The name of the property for which the errors changed, or <code>null</code> or <see cref="String.Empty">Empty</see> 
+        /// for entity-level errors.
+        /// </param>
+        /// <returns>The validation errors for the property or entity.</returns>
         IEnumerable GetErrors(string propertyName);
 
+        /// <summary>
+        /// Gets a value that indicates whether the entity has validation errors.
+        /// </summary>
+        /// <remarks>
+        /// This property returns false if there are no known entity-level or property-level validation errors for the entity at 
+        /// the time it is accessed. However, some validation rules may still be running asynchronously as described for the 
+        /// <see cref="GetErrors"/> method. 
+        /// </remarks>
+        /// <value><code>true</code> if the entity currently has validation errors; otherwise, <code>false</code>.</value>
         bool HasErrors { get; }
     }
 }
